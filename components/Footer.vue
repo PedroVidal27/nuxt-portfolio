74a5d455d5f3f9d4edb1419c485822429c9748ff
PedroVidal27 <pedrovidal2703@gmail.com>
2025-01-10 08:55:31 -0300

    :sparkles: add disclaimer about logos and photos used

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,6 +2,9 @@
   <footer class="bg-primary text-white py-4">
     <div class="container mx-auto text-center">
       <p>© {{ new Date().getFullYear() }} Pedro Vidal - Todos os Direitos Reservados</p>
+      <p class="text-sm mt-2">
+        Este site utiliza recursos de terceiros. Todos os direitos sobre esses recursos pertencem aos seus respectivos criadores.
+      </p>
     </div>
   </footer>
 </template>
